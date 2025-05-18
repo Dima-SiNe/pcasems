bd2533193b38d1bb2d3a8086532f835087253f61
Dima-SiNe <116962059+Dima-SiNe@users.noreply.github.com>
2025-05-18 16:40:17 +0300

    Now I think 100% I fixed the transitions

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/pcasems/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        simulator: resolve(__dirname, 'simulator.html'),
+      },
+    },
+  },
 })
